f82887eb9913db8e8acd341ed0aa0df94e3defb5
dinglei <sx-dingl@dtdream.com>
2017-12-29 17:00:11 +0800

    解决页数切换的问题

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -22,13 +22,13 @@ export default class Pagination extends React.Component {
           <div key={`pagination-${2}`} onClick={() => { this.props.callback(2) }} className={`pagination-num-specific ${pageNo === 2 ? 'selected' : ''}`}>{2}</div>,
           <div key={`pagination-${3}`} onClick={() => { this.props.callback(3) }} className={`pagination-num-specific ${pageNo === 3 ? 'selected' : ''}`}>{3}</div>,
           <div key={`pagination-${4}`} onClick={() => { this.props.callback(4) }} className={`pagination-num-specific ${pageNo === 4 ? 'selected' : ''}`}>{4}</div>,
-          <span key='ellipsis-1'>...</span>,
+          <span className='ellipsis' key='ellipsis-1'>...</span>,
           <div key={`pagination-${pageTotal}`} onClick={() => { this.props.callback(pageTotal) }} className={`pagination-num-specific ${pageNo === pageTotal ? 'selected' : ''}`}>{pageTotal}</div>
         ]
       } else if (pageNo > pageTotal - 3) {
         list = [
           <div key={`pagination-${1}`} onClick={() => { this.props.callback(1) }} className={`pagination-num-specific ${pageNo === 1 ? 'selected' : ''}`}>{1}</div>,
-          <span key='ellipsis-1'>...</span>,
+          <span className='ellipsis' key='ellipsis-1'>...</span>,
           <div key={`pagination-${pageTotal - 3}`} onClick={() => { this.props.callback(pageTotal - 3) }} className={`pagination-num-specific ${pageNo === pageTotal - 3 ? 'selected' : ''}`}>{pageTotal - 3}</div>,
           <div key={`pagination-${pageTotal - 2}`} onClick={() => { this.props.callback(pageTotal - 2) }} className={`pagination-num-specific ${pageNo === pageTotal - 2 ? 'selected' : ''}`}>{pageTotal - 2}</div>,
           <div key={`pagination-${pageTotal - 1}`} onClick={() => { this.props.callback(pageTotal - 1) }} className={`pagination-num-specific ${pageNo === pageTotal - 1 ? 'selected' : ''}`}>{pageTotal - 1}</div>,
@@ -37,11 +37,11 @@ export default class Pagination extends React.Component {
       } else {
         list = [
           <div key={`pagination-${1}`} onClick={() => { this.props.callback(1) }} className={`pagination-num-specific ${pageNo === 1 ? 'selected' : ''}`}>{1}</div>,
-          <span key='ellipsis-1'>...</span>,
+          <span className='ellipsis' key='ellipsis-1'>...</span>,
           <div key={`pagination-${pageNo - 1}`} onClick={() => { this.props.callback(pageNo - 1) }} className={`pagination-num-specific`}>{pageNo - 1}</div>,
           <div key={`pagination-${pageNo}`} onClick={() => { this.props.callback(pageNo) }} className={`pagination-num-specific selected`}>{pageNo}</div>,
           <div key={`pagination-${pageNo + 1}`} onClick={() => { this.props.callback(pageNo + 1) }} className={`pagination-num-specific`}>{pageNo + 1}</div>,
-          <span key='ellipsis-2'>...</span>,
+          <span className='ellipsis' key='ellipsis-2'>...</span>,
           <div key={`pagination-${pageTotal}`} onClick={() => { this.props.callback(pageTotal) }} className={`pagination-num-specific ${pageNo === pageTotal ? 'selected' : ''}`}>{pageTotal}</div>
         ]
       }
@@ -55,12 +55,12 @@ export default class Pagination extends React.Component {
       <article className='pagination-root'>
         {/* <div className='pagination-btn pagination-backward' /> */}
         <img onClick={() => { this.props.callback(1) }} className='pagination-btn pagination-backward' src={require('../../assets/leftgotiny.png')} alt='' />
-        <img onClick={() => { this.props.callback(this.props.pageNo - 1) }} className='pagination-btn pagination-backward' src={require('../../assets/leftgo.png')} alt='' />
+        <img onClick={() => { this.props.callback(this.props.pageNo > 1 ? this.props.pageNo - 1 : 1) }} className='pagination-btn pagination-backward' src={require('../../assets/leftgo.png')} alt='' />
         <div className='pagination-num'>
           {this.producePage(this.props.pageSize, this.props.total, this.props.pageNo)}
         </div>
         {/* <div className='pagination-btn pagination-forward' /> */}
-        <img onClick={() => { this.props.callback(this.props.pageNo + 1) }} className='pagination-btn pagination-forward' src={require('../../assets/leftgo.png')} alt='' />
+        <img onClick={() => { this.props.callback(this.props.pageNo >= Math.ceil(this.props.total / this.props.pageSize) ? Math.ceil(this.props.total / this.props.pageSize) : this.props.pageNo + 1) }} className='pagination-btn pagination-forward' src={require('../../assets/leftgo.png')} alt='' />
         <img onClick={() => { this.props.callback(Math.ceil(this.props.total / this.props.pageSize)) }} className='pagination-btn pagination-forward' src={require('../../assets/leftgotiny.png')} alt='' />
       </article>
     )
